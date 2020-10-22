1cf5eaa2875923b98a7cc8f22365cef70e91ecc3
Aina <s338849@oslomet.no>
2020-10-22 12:55:04 +0200

    Oppgave 2 er nå i orden. Det var kallene i Main som var problemet. Bare litt blingsing

--- src/no/oslomet/cs/algdat/Eksamen/Main.java
+++ src/no/oslomet/cs/algdat/Eksamen/Main.java
@@ -9,19 +9,23 @@ public class Main {
         EksamenSBinTre<Integer> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
         System.out.println(tre.antall()); // Utskrift: 0
 
+        //// Oppgave 1 ////
         Integer[] a = {4,7,2,9,5,10,8,1,3,6};
         for (int verdi : a) {
             tre.leggInn(verdi);
         }
-
-        //// Oppgave 1 ////
         System.out.println(tre.antall());  // Utskrift: 10
 
         //// Oppgave 2 ////
-        System.out.println(tre.antall(5));  // Utskrift: 0
-        System.out.println(tre.antall(4));  // Utskrift: 3
-        System.out.println(tre.antall(7));  // Utskrift: 2
-        System.out.println(tre.antall(10));  // Utskrift: 1
+        Integer[] b = {4,7,2,9,4,10,8,7,4,6};
+        EksamenSBinTre<Integer> tre2 = new EksamenSBinTre<>(Comparator.naturalOrder());
+        for (int verdi : b) {
+            tre2.leggInn(verdi);
+        }
+        System.out.println(tre2.antall(5));  // Utskrift: 0
+        System.out.println(tre2.antall(4));  // Utskrift: 3
+        System.out.println(tre2.antall(7));  // Utskrift: 2
+        System.out.println(tre2.antall(10));  // Utskrift: 1
 
 
     }
